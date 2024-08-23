0586b262336b11ef4445435bd5a2785b37e6a00f
L3xtor <ostertag.levin@gmail.com>
2024-08-23 11:49:15 +0200

    Added the functionality of getting the Emoji Markdown from Bot Dashboard via API

--- cogs/DiscordGetEmoji.py
+++ cogs/DiscordGetEmoji.py
@@ -1,17 +1,18 @@
 import requests
-from utils.settings import DISCORD_API_SECRET
 
-def gete(itemname):
+def gete(itemname, DISCORD_API_SECRET):
 
     applicationurl = ('https://discord.com/api/v10/applications/1264605196466651249/emojis')
 
     headers = {'Authorization':'Bot ' + DISCORD_API_SECRET}
     data = (requests.get(applicationurl, headers=headers)).json()
-
+    
     for emote in data['items']:
         if emote['name'] == itemname:
             emoteid = (emote['id'])
             markdown = (f'<:{itemname}:{emoteid}>')
+            print(markdown)
         break
     else:
         print ('not found')
+
