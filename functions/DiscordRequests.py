075e84984b0c7b2b2fbd771113b2d44e8d2f99e7
CodingWithFiery <sakshamthecoder@gmail.com>
2024-08-29 22:37:10 +0530

    Fixed the shit code

--- functions/DiscordRequests.py
+++ functions/DiscordRequests.py
@@ -1,25 +1,24 @@
 import requests
-import json
+from ..utils.settings import DISCORD_API_SECRET
 
 
-def getemote(itemname, apikey):
+def getemote(itemname, apikey = DISCORD_API_SECRET):
     applicationurl = ('https://discord.com/api/v10/applications/1264605196466651249/emojis')
     headers = {'Authorization':'Bot ' + apikey}
     request = requests.get(applicationurl, headers=headers)
-    data = (request).json()
+    if request.ok: 
+        data = request.json()
+    
+    else:
+        raise ValueError('Request is not ok, verify key', request.content)
 
 
 # Iterate through the items and print the "name"
-    if data['items'][0]['name'] == itemname:
-       emote_name = data['items'][0]['name']
-       print('here')
-       emote_id = emote_name['id'] 
+    item = data['items'][0]
+
+    if item['name'] == itemname:
+       emote_id = item['id'] 
        markdown= f'<:{itemname}:{emote_id}>'
        return markdown
 
-   
- 
-    
-
-    
-
+print(getemote('End_Sword'))
