313ffccd6eafd45c4d16c0a4020c5202ec222386
CodingWithFiery <sakshamthecoder@gmail.com>
2024-08-30 11:41:51 +0530

    Made it readable

--- cogs/Emoji.py
+++ cogs/Emoji.py
@@ -1,26 +1,27 @@
-import requests
+
 from discord.ext import commands
-from utils.settings import DISCORD_API_SECRET as k
+from utils.settings import DISCORD_API_SECRET
 from functions.DiscordRequests import getemote as gete
 from functions .GetItemJPG import getjpg as getj
 from functions.DiscordPosts import postdiscordemote as postde
 
-class emotes(commands.Cog):
+class Emotes(commands.Cog):
 
 	@commands.hybrid_command()
 	async def gete(self, ctx, itemname: str):
-		markdown = gete(itemname, k)
+		markdown = gete(itemname, DISCORD_API_SECRET)
 		await ctx.send(f'The Emoji: {markdown}') 
           
+
 	@commands.hybrid_command()
 	async def createe(self, ctx, itemname: str):
 		getj(itemname)
 		jpg = f'Emoji_Images/{itemname}.jpg'
-		post = postde(jpg, itemname, k)
-		markdown = gete(itemname, k)
+		post = postde(jpg, itemname, DISCORD_API_SECRET)
+		markdown = gete(itemname, DISCORD_API_SECRET)
         
 		await ctx.send(f'The Emoji: {markdown}')
 
 
 async def setup(bot: commands.Bot):
-   await bot.add_cog(emotes(bot))
+   await bot.add_cog(Emotes(bot))
