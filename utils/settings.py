3164f86b672db225b47d9560c8753f019098dd58
CodingWithFiery <sakshamthecoder@gmail.com>
2024-09-14 00:58:07 +0530

    Lol

--- utils/settings.py
+++ utils/settings.py
@@ -14,9 +14,13 @@ DISCORD_API_SECRET = getenv("DISCORD_API_TOKEN")
 HYPIXEL_API_SECRET = getenv("HYPIXEL_API_TOKEN")
 LOGGING_CHANNEL = getenv('LOGGING_CHANNEL')
 
-COGS_DIR = listdir('cogs')
+NORMAL_COGS_DIR = listdir('cogs')
+HYPIXEL_COGS_DIR = listdir('hypixel')
 
 # Adds ever cog into the list without '.py' and also ignores the file if its a '__init__.py' file
-COGS = [f'cogs.{cogs[:-3]}' for cogs in COGS_DIR if cogs != '__init__.py' and cogs.endswith('.py') ]
+NORMAL_COGS = [f'cogs.{cogs[:-3]}' for cogs in NORMAL_COGS_DIR if cogs != '__init__.py' and cogs.endswith('.py') ]
+HYPIXEL_COGS = [f'hypixel.{cogs[:-3]}' for cogs in HYPIXEL_COGS_DIR if cogs != '__init__.py' and cogs.endswith('.py') ]
+
+COGS = NORMAL_COGS + HYPIXEL_COGS
 
 dictConfig(logging_config)
