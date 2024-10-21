e4ae46e6d1463b0d3adef5047b99412690693cf9
L3xtor <ostertag.levin@gmail.com>
2024-10-21 12:31:48 +0200

    added the fishing folder to cogs

--- utils/settings.py
+++ utils/settings.py
@@ -16,12 +16,14 @@ LOGGING_CHANNEL = getenv('LOGGING_CHANNEL')
 NORMAL_COGS_DIR = listdir('cogs')
 HYPIXEL_COGS_DIR = listdir('hypixel')
 CATACOMBS_COGS_DIR = listdir('hypixel/catacombs')
+FISHING_COGS_DIR = listdir('hypixel/fishing')
 
 # Adds ever cog into the list without '.py' and also ignores the file if its a '__init__.py' file
 NORMAL_COGS = [f'cogs.{cogs[:-3]}' for cogs in NORMAL_COGS_DIR if cogs != '__init__.py' and cogs.endswith('.py') ]
 HYPIXEL_COGS = [f'hypixel.{cogs[:-3]}' for cogs in HYPIXEL_COGS_DIR if cogs != '__init__.py' and cogs.endswith('.py') ]
 CATACOMBS_COGS = [f'hypixel.catacombs.{cogs[:-3]}' for cogs in CATACOMBS_COGS_DIR if cogs != '__init__.py' and cogs.endswith('.py') ]
+FISHING_COGS = [f'hypixel.fishing.{cogs[:-3]}' for cogs in FISHING_COGS_DIR if cogs != '__init__.py' and cogs.endswith('.py') ]
 
-COGS = NORMAL_COGS + HYPIXEL_COGS + CATACOMBS_COGS
+COGS = NORMAL_COGS + HYPIXEL_COGS + CATACOMBS_COGS + FISHING_COGS
 
 dictConfig(logging_config)
