e4285f5b0283202a47ae001efa1850a826b478bd
L3xtor <ostertag.levin@gmail.com>
2024-10-22 12:20:32 +0200

    added the new general folder to cogs

--- utils/settings.py
+++ utils/settings.py
@@ -17,13 +17,15 @@ NORMAL_COGS_DIR = listdir('cogs')
 HYPIXEL_COGS_DIR = listdir('hypixel')
 CATACOMBS_COGS_DIR = listdir('hypixel/catacombs')
 FISHING_COGS_DIR = listdir('hypixel/fishing')
+GENERAL_COGS_DIR = listdir('hypixel/general')
 
 # Adds ever cog into the list without '.py' and also ignores the file if its a '__init__.py' file
 NORMAL_COGS = [f'cogs.{cogs[:-3]}' for cogs in NORMAL_COGS_DIR if cogs != '__init__.py' and cogs.endswith('.py') ]
 HYPIXEL_COGS = [f'hypixel.{cogs[:-3]}' for cogs in HYPIXEL_COGS_DIR if cogs != '__init__.py' and cogs.endswith('.py') ]
 CATACOMBS_COGS = [f'hypixel.catacombs.{cogs[:-3]}' for cogs in CATACOMBS_COGS_DIR if cogs != '__init__.py' and cogs.endswith('.py') ]
 FISHING_COGS = [f'hypixel.fishing.{cogs[:-3]}' for cogs in FISHING_COGS_DIR if cogs != '__init__.py' and cogs.endswith('.py') ]
+GENERAL_COGS = [f'hypixel.general.{cogs[:-3]}' for cogs in GENERAL_COGS_DIR if cogs != '__init__.py' and cogs.endswith('.py') ]
 
-COGS = NORMAL_COGS + HYPIXEL_COGS + CATACOMBS_COGS + FISHING_COGS
+COGS = NORMAL_COGS + HYPIXEL_COGS + CATACOMBS_COGS + FISHING_COGS + GENERAL_COGS
 
 dictConfig(logging_config)
