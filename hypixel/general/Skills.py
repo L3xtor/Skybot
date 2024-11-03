9aa1e6ac70a327156ff50d7f2ac04c0bb0cb30cb
L3xtor <ostertag.levin@gmail.com>
2024-11-03 21:03:38 +0100

    added this

--- hypixel/general/Skills.py
+++ hypixel/general/Skills.py
@@ -13,17 +13,22 @@ class Skills(commands.Cog):
 
 			while not view.interaction_complete: await asyncio.sleep(1)  # Loop until the user has interacted
 
-			return view.skill_data  # Return the selected skill data
+			return view.skill_data, view.skill_name   # Return the selected skill data
 
-		skilldata = await getskillxp(playername=playername)
-		print(skilldata)
+		skilldata, skillname = await getskillxp(playername=playername)  
+		
+		if skillname == "Catacombs":
+			skilltype = skillname
+		else:
+			skilltype = "Skill"
 
 		embed = discord.Embed(
 		  color = discord.Color.dark_teal(),
 		  title = f"Stat-Breakdown for {playername.title()}",
 		)
 
-		skillLevel, current_xp, xp_to_reach_next_level = getPlayerSkillLevel(skilldata)
+		skillLevel, current_xp, xp_to_reach_next_level = getPlayerSkillLevel(skilldata, skilltype)
+		embed.add_field(name='**Skill Name**', value= skillname, inline=False)
 		embed.add_field(name='**Skill Level**', value= skillLevel, inline=False)
 		embed.add_field(name=f'Current xp: {current_xp}', inline=False)
 		embed.add_field(name='xp to reach next Level:', value= xp_to_reach_next_level, inline=False)
