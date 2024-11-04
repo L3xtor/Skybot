d95e3fce09b4a6a1d3ac2d8cc206604cf139ae38
L3xtor <ostertag.levin@gmail.com>
2024-11-04 14:06:56 +0100

    Changed the Skills Command to actually work

--- hypixel/general/Skills.py
+++ hypixel/general/Skills.py
@@ -3,6 +3,8 @@ import asyncio
 from discord.ext import commands
 from hypixel.utils.Skillselection import Skillselection
 from hypixel.utils.rename_this_shit import getPlayerSkillLevel
+from numerize.numerize import numerize
+
 
 class Skills(commands.Cog):
 	@commands.hybrid_command()
@@ -15,23 +17,27 @@ class Skills(commands.Cog):
 
 			return view.skill_data, view.skill_name   # Return the selected skill data
 
-		skilldata, skillname = await getskillxp(playername=playername)  
+		skilldata, skillname, skillemote = await getskillxp(playername=playername)  
 		
 		if skillname == "Catacombs":
 			skilltype = skillname
+			SkillnameFormated = skillname.capitalize()
 		else:
-			skilltype = "Skill"
+			skilltype = "Skills"
+			SkillnameFormated = skillname.replace("SKILL_", "").capitalize()
+
 
 		embed = discord.Embed(
 		  color = discord.Color.dark_teal(),
-		  title = f"Stat-Breakdown for {playername.title()}",
+		  title = f"Skill-Breakdown for {playername.title()}",
 		)
 
 		skillLevel, current_xp, xp_to_reach_next_level = getPlayerSkillLevel(skilldata, skilltype)
-		embed.add_field(name='**Skill Name**', value= skillname, inline=False)
+
+		embed.add_field(name='**Selected Skill**', value= (skillemote, "", SkillnameFormated), inline=False)
 		embed.add_field(name='**Skill Level**', value= skillLevel, inline=False)
-		embed.add_field(name=f'Current xp: {current_xp}', inline=False)
-		embed.add_field(name='xp to reach next Level:', value= xp_to_reach_next_level, inline=False)
+		embed.add_field(name=f'Current xp:', value=numerize(current_xp), inline=False)
+		embed.add_field(name='xp to reach next Level:', value= numerize(xp_to_reach_next_level), inline=False)
 
 		await ctx.send(embed=embed)
 
