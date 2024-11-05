b2997965b5a52f3f58d2a030beda9cb68e5e3e97
L3xtor <ostertag.levin@gmail.com>
2024-11-05 12:33:06 +0100

    -Corrected the Impot to the new Name -Fixed the Emote Import

--- hypixel/general/Skills.py
+++ hypixel/general/Skills.py
@@ -2,7 +2,7 @@ import discord
 import asyncio
 from discord.ext import commands
 from hypixel.utils.Skillselection import Skillselection
-from hypixel.utils.rename_this_shit import getPlayerSkillLevel
+from hypixel.utils.GetSkillInfo import getPlayerSkillLevel
 from numerize.numerize import numerize
 
 
@@ -15,7 +15,7 @@ class Skills(commands.Cog):
 
 			while not view.interaction_complete: await asyncio.sleep(1)  # Loop until the user has interacted
 
-			return view.skill_data, view.skill_name   # Return the selected skill data
+			return view.skill_data, view.skill_name, view.skill_emoji   # Return the selected skill data
 
 		skilldata, skillname, skillemote = await getskillxp(playername=playername)  
 		
@@ -34,7 +34,7 @@ class Skills(commands.Cog):
 
 		skillLevel, current_xp, xp_to_reach_next_level = getPlayerSkillLevel(skilldata, skilltype)
 
-		embed.add_field(name='**Selected Skill**', value= (skillemote, "", SkillnameFormated), inline=False)
+		embed.add_field(name='**Selected Skill**', value= f'[{skillemote}] {SkillnameFormated}', inline=False) 
 		embed.add_field(name='**Skill Level**', value= skillLevel, inline=False)
 		embed.add_field(name=f'Current xp:', value=numerize(current_xp), inline=False)
 		embed.add_field(name='xp to reach next Level:', value= numerize(xp_to_reach_next_level), inline=False)
